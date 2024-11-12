efe293e40fdbf9cc7ac3bf019c1eea53321951c1
Arvvvy <arvanyodeyai@gmail.com>
2024-11-12 12:39:58 +0900

    Update git.js

--- git.js
+++ git.js
@@ -3,7 +3,7 @@ let model;
 async function loadModel() {
     if (!model) {
         try {
-            model = await tf.loadGraphModel('https://raw.githubusercontent.com/Arvvvy/eggking/main/model/model.json'); // Sesuaikan path model
+            model = await tf.loadGraphModel('https://cors-anywhere.herokuapp.com/https://raw.githubusercontent.com/Arvvvy/eggking/main/model/model.json'); // Sesuaikan path model
             console.log('Model loaded successfully');
         } catch (error) {
             console.error('Error loading model:', error);
